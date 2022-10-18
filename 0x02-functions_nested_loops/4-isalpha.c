ddaaad0e7e538d6e4eaf128932246ed29956244f
Nosihle <Sihlegabimbatha@gmail.com>
2022-10-18 05:36:34 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign -print the sign of a num
+ * @n: sum to be checked for
+ * Return: 1 and print 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return ('-1');
+	}
+}
