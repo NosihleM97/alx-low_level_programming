e53b97120cf56efd857be38ced5e78d7ce0a965d
Nosihle <Sihlegabimbatha@gmail.com>
2022-10-18 03:51:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
  * main - print _putchar
- *Return: always 0
+ * Return: always 0
  */
 
 int main(void)
@@ -17,4 +18,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-
